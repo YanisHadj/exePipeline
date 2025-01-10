8966bd15667387816d8eb4feefb5c80376f935e2
Your Name <you@example.com>
2025-01-10 15:39:36 +0100

    last test

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
-// Package main contient l'application principale du pipeline CI/CD.
 package main
 
 import "fmt"
 
-// Main est la fonction principale qui lance le pipeline CI/CD.
 func main() {
-    // Exécution de la tâche de build
-    fmt.Println("Le pipeline CI/CD est en cours d'exécution...")
+    fmt.Println("Hello, Go CI/CD!")
 }
